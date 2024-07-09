88981c32531448be378c6a1f42a5dede5cfde90e
Aaron Windsor <aaron.windsor@gmail.com>
2024-07-09 08:14:11 -0400

    working on some examples

--- millisat.py
+++ millisat.py
@@ -18,30 +18,34 @@ class Solver:
         self.assign = {} # var -> True/False
 
     def add_clause(self, c):
-        # mem looks like: [watch1] [watch2] [size] [lit1] [lit2] [lit3]
         c = list(set(c))
         self.nvars = max(self.nvars, *(abs(x) for x in c))
         x, y = c[0], c[1]
-        w1 = self.watch.get(x, 0) if len(c) > 1 else -1
-        w2 = self.watch.get(y, 0) if len(c) > 1 else -1
+        w1 = self.watch.get(x, -1) if len(c) > 1 else -1
+        w2 = self.watch.get(y, -1) if len(c) > 1 else -1
         self.clauses.append(Clause(x,y,c,{x: w1, y: w2}))
         if len(c) > 1:
+            print('Setting watch[{}] = watch[{}] = {}'.format(x,y,len(self.clauses)-1))
             self.watch[x], self.watch[y] = len(self.clauses)-1, len(self.clauses)-1
         else:
             self.units.add(x)
 
     def solve(self):
-        trail = [(v,None) for v in self.units]  # pairs of (var, reason)
+        trail = [(v,None,0) for v in self.units]  # tuples of (lit, reason, level)
+        curr_level = 0
         levels = [0]  # maps level # -> start index in trail
+        level = {}
         tp = 0  # Next unprocessed trail item
         while len(self.assign) < self.nvars:
             print('assignments: {}'.format(self.assign))
             # Propagate pending implications
             while tp < len(trail):
-                wl, reason = trail[tp]
-                print('Propagating {} (reason: {})'.format(wl, reason))
-                wp = self.watch.get(-wl, 0)
-                while wp > 0:
+                wl, reason, _ = trail[tp]
+                print('Propagating {} (reason: {}) (wp for {}: {})'.format(wl, reason, -wl, self.watch.get(-wl,-1)))
+                print('Trail: {}'.format(trail))
+                level[abs(wl)] = curr_level
+                wp = self.watch.get(-wl, -1)
+                while wp >= 0:
                     # Try to find another lit to watch
                     clause = self.clauses[wp]
                     print('  Finding another watch for {} except {}'.format(clause.lits, clause.watches))
@@ -56,26 +60,62 @@ class Solver:
                     if -wl in clause.watches:
                         forced = (clause.watches - {-wl}).pop()
                         if self.assign.get(abs(forced)) == (forced < 0):
-                            print('Conflict with {} and trail: {}'.format(forced, trail))
-                            print('self.assign.get(abs({})) = {}'.format(forced, self.assign.get(abs(forced))))
-                            import sys; sys.exit(-1)
+                            print('Conflict with lit {}, clause {} and trail: {}. Resolving...'.format(forced, clause, trail))
+                            if curr_level == 0: return False  # UNSAT
+                            stamp = dict((-l, True) for l in clause.lits)
+                            resolved = set(clause.lits)
+                            trail.append((forced, clause, curr_level))
+                            backjump_level = curr_level
+                            for tl, tc, _ in reversed(trail):
+                                if tc is None: continue
+                                if stamp.get(tl):
+                                    for l in tc.lits:
+                                        stamp[-l] = True
+                                        if -l in resolved:
+                                            resolved.remove(-l)
+                                        else:
+                                            resolved.add(l)
+                                    lcount = sum(1 for l in resolved if level[abs(l)] == curr_level)
+                                    if lcount == 1:
+                                        print('FINAL RESOLVED: {}'.format(resolved))
+                                        backjump_level = max((l for l in resolved if level[abs(l)] < curr_level), default=0)
+                                        print('NEW LEVEL: {}'.format(backjump_level))
+                                        break
+                            new_l = [l for l in resolved if level[abs(l)] == curr_level][0]
+                            while trail and trail[-1][-1] > backjump_level:
+                                l, r, _  = trail.pop()
+                                if abs(l) in self.assign: del self.assign[abs(l)]
+                                if abs(l) in level: del level[abs(l)]
+                            tp = len(trail)-1
+                            self.assign[abs(new_l)] = new_l > 0
+                            trail.append((new_l, list(resolved), backjump_level))
+                            level[abs(new_l)] = backjump_level
+                            curr_level = backjump_level
+                            break
                         elif self.assign.get(abs(forced)) == (forced > 0):
                             print('  {} already true, moving on...'.format(forced))
                         else:
                             print('  {} forced by {}, adding to trail and assigning'.format(forced, clause.lits))
                             self.assign[abs(forced)] = forced > 0
-                            trail.append((forced, clause))
+                            trail.append((forced, clause, curr_level))
+                            level[abs(forced)] = curr_level
                     wp = clause.wnext[-wl]
                     print('  Moving on to wp = {}'.format(wp))
                 print('  Done exploring watch list for {}'.format(-wl))
                 tp += 1
 
+            if len(self.assign) == self.nvars: break
+
             # Start a new level
             v = (range(1,self.nvars+1) - self.assign.keys()).pop()
+            print('Trail: {}'.format(trail))
             print('Choosing {}'.format(v))  # TODO: default to negative polarity
             self.assign[v] = False
+            curr_level += 1
+            level[v] = curr_level
             levels.append(len(trail))
-            trail.append((v, None))
+            trail.append((v, None, curr_level))
+
         return True
 
 def parse_dimacs(s):
@@ -116,4 +156,11 @@ if __name__ == '__main__':
     s = Solver()
     for c in clauses:
         s.add_clause(c)
-    s.solve()
+    if s.solve():
+        assignments = [l * (1 if v else -1) for l,v in s.assign.items()]
+        stride = 10
+        for i in range(0, len(assignments), stride):
+            print('v {} 0'.format(' '.join((str(x) for x in assignments[i:i+stride]))))
+        print('s SATISFIABLE')
+    else:
+        print('s UNSATISFIABLE')
